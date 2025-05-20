4ca509a1b0c245059c150b28b826a604feb26340
Eduardo de Figueiredo Costa <edfcosta@inf.ufpel.edu.br>
2025-05-20 14:33:27 -0300

    SUB e DIV funcionando

--- AnaliseLexica.java
+++ AnaliseLexica.java
@@ -1,13 +1,17 @@
 import java.io.*;
 
-enum TokenType{ NUM, SOMA, MULT, SUB, DIV, APar,FPar, EOF}
+enum TokenType {
+	NUM, SOMA, MULT, SUB, DIV, APar, FPar, EOF
+}
 
-class Token{
-  char lexema;
-  TokenType token;
+class Token {
+	char lexema;
+	TokenType token;
 
- Token (char l, TokenType t)
- 	{ lexema=l;token = t;}	
+	Token(char l, TokenType t) {
+		lexema = l;
+		token = t;
+	}
 
 }
 
@@ -15,52 +19,49 @@ class AnaliseLexica {
 
 	BufferedReader arquivo;
 
-	AnaliseLexica(String a) throws Exception
-	{
-		
-	 	this.arquivo = new BufferedReader(new FileReader(a));
-		
+	AnaliseLexica(String a) throws Exception {
+
+		this.arquivo = new BufferedReader(new FileReader(a));
+
 	}
 
-	Token getNextToken() throws Exception
-	{	
+	Token getNextToken() throws Exception {
 		Token token;
 		int eof = -1;
 		char currchar;
 		int currchar1;
 
-			do{
-				currchar1 =  arquivo.read();
-				currchar = (char) currchar1;
-			} while (currchar == '\n' || currchar == ' ' || currchar =='\t' || currchar == '\r');
-			
-			if(currchar1 != eof && currchar1 !=10)
-			{
-								
-	
-				if (currchar >= '0' && currchar <= '9')
-					return (new Token (currchar, TokenType.NUM));
-				else
-					switch (currchar){
-						case '(':
-							return (new Token (currchar,TokenType.APar));
-						case ')':
-							return (new Token (currchar,TokenType.FPar));
-						case '+':
-							return (new Token (currchar,TokenType.SOMA));
-						case '*':
-							return (new Token (currchar,TokenType.MULT));
-						case '-':
-                                                        return (new Token (currchar,TokenType.SUB));
-						case '/':
-                                                        return (new Token (currchar,TokenType.DIV));
-						default: throw (new Exception("Caractere inválido: " + ((int) currchar)));
-					}
-			}
+		do {
+			currchar1 = arquivo.read();
+			currchar = (char) currchar1;
+		} while (currchar == '\n' || currchar == ' ' || currchar == '\t' || currchar == '\r');
 
-			arquivo.close();
-			
-		return (new Token(currchar,TokenType.EOF));
-		
+		if (currchar1 != eof && currchar1 != 10) {
+
+			if (currchar >= '0' && currchar <= '9')
+				return (new Token(currchar, TokenType.NUM));
+			else
+				switch (currchar) {
+					case '(':
+						return (new Token(currchar, TokenType.APar));
+					case ')':
+						return (new Token(currchar, TokenType.FPar));
+					case '+':
+						return (new Token(currchar, TokenType.SOMA));
+					case '*':
+						return (new Token(currchar, TokenType.MULT));
+					case '-':
+						return (new Token(currchar, TokenType.SUB));
+					case '/':
+						return (new Token(currchar, TokenType.DIV));
+					default:
+						throw (new Exception("Caractere inválido: " + ((int) currchar)));
+				}
+		}
+
+		arquivo.close();
+
+		return (new Token(currchar, TokenType.EOF));
+
 	}
 }
