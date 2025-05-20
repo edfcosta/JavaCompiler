cb3c923ac2c0a1cd9cbcf85becb52a8f60d32f83
Eduardo de Figueiredo Costa <edfcosta@inf.ufpel.edu.br>
2025-05-20 15:12:49 -0300

    numeros de 1 dígito+ e SUB e DIV

--- AnaliseLexica.java
+++ AnaliseLexica.java
@@ -5,10 +5,10 @@ enum TokenType {
 }
 
 class Token {
-	char lexema;
+	String lexema;
 	TokenType token;
 
-	Token(char l, TokenType t) {
+	Token(String l, TokenType t) {
 		lexema = l;
 		token = t;
 	}
@@ -38,22 +38,40 @@ class AnaliseLexica {
 
 		if (currchar1 != eof && currchar1 != 10) {
 
-			if (currchar >= '0' && currchar <= '9')
-				return (new Token(currchar, TokenType.NUM));
-			else
+			if (currchar >= '0' && currchar <= '9') {
+				StringBuilder lexema = new StringBuilder();
+				lexema.append(currchar);
+
+				while (true) {
+					arquivo.mark(1);
+					currchar1 = arquivo.read();
+					if (currchar1 == -1)
+						break;
+
+					currchar = (char) currchar1;
+
+					if (currchar >= '0' && currchar <= '9') {
+						lexema.append(currchar);
+					} else {
+						arquivo.reset();
+						break;
+					}
+				}
+				return new Token(lexema.toString(), TokenType.NUM);
+			} else
 				switch (currchar) {
 					case '(':
-						return (new Token(currchar, TokenType.APar));
+						return (new Token("" + currchar, TokenType.APar));
 					case ')':
-						return (new Token(currchar, TokenType.FPar));
+						return (new Token("" + currchar, TokenType.FPar));
 					case '+':
-						return (new Token(currchar, TokenType.SOMA));
+						return (new Token("" + currchar, TokenType.SOMA));
 					case '*':
-						return (new Token(currchar, TokenType.MULT));
+						return (new Token("" + currchar, TokenType.MULT));
 					case '-':
-						return (new Token(currchar, TokenType.SUB));
+						return (new Token("" + currchar, TokenType.SUB));
 					case '/':
-						return (new Token(currchar, TokenType.DIV));
+						return (new Token("" + currchar, TokenType.DIV));
 					default:
 						throw (new Exception("Caractere inválido: " + ((int) currchar)));
 				}
@@ -61,7 +79,7 @@ class AnaliseLexica {
 
 		arquivo.close();
 
-		return (new Token(currchar, TokenType.EOF));
+		return (new Token("" + currchar, TokenType.EOF));
 
 	}
 }
